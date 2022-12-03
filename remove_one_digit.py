32f2780fc0ca0cf897967750d763ed3fd49497f9
Stan Dragos <standragos82@gmail.com>
2022-12-03 20:56:37 +0200

    optimized remove_one_digit

--- remove_one_digit.py
+++ remove_one_digit.py
@@ -8,16 +8,10 @@ class Solution:
                 digit_index.append(index)
             index += 1
         num = [int(x) for x in str(number)]
-        print(num)
-        print(digit_index)
         for i in digit_index:
             num = [int(x) for x in str(number)]
             num.pop(i)
-            print(num)
             result.append(int("".join(map(str, num))))
-            print(result)
-            result.sort()
-
         return str(max(result))
         
 
