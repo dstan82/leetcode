d1508348aa0b95a00b07a567add55d5fa6544f2a
Stan Dragos <standragos82@gmail.com>
2022-12-04 12:01:37 +0200

    final final commit 3 methods faster 97%

--- minimum_average_diference.py
+++ minimum_average_diference.py
@@ -1,7 +1,7 @@
 from math import floor
 import time
 
-
+'''
 #Method1
 class Solution:
     def minimumAverageDifference(self, nums: list[int]) -> int:
@@ -18,7 +18,7 @@ class Solution:
             average2 = floor(sum2 / (total_length-index) if total_length-index != 0 else 0)
             diff[index-1] = abs(average2-average1)
         return min(diff, key=diff.get)
-'''
+
 #Method2
 class Solution:
     def minimumAverageDifference(self, nums: list[int]) -> int:
@@ -38,6 +38,24 @@ class Solution:
                 result = index - 1
         return result
 '''
+
+#Method3
+class Solution:
+    def minimumAverageDifference(self, nums: list[int]) -> int:
+        index = 0
+        sum1 = 0
+        total_sum = sum(nums)
+        total_length = len(nums)
+        diff = {}
+        for num in nums:
+            sum1 += num
+            average1 = sum1//(index+1)
+            sum2 = total_sum - sum1
+            index += 1
+            average2 = sum2 // (total_length-index) if total_length-index != 0 else 0
+            diff[index-1] = abs(average2-average1)
+        return min(diff, key=diff.get)
+
 
 obj = Solution()
 list0 = [2,5,3,9,5,3]
