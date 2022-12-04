e73b0e2b8f63ed3c7533b60816d16f706c06e36a
Stan Dragos <standragos82@gmail.com>
2022-12-04 09:23:12 +0200

    second step

--- minimum_average_diference.py
+++ minimum_average_diference.py
@@ -6,9 +6,14 @@ class Solution:
         for i in range(len(nums)):
             a = sum(nums[0:i+1])
             b = len(nums[0:i+1])
-            c = floor(a/b)
-            print(c)
-        
+            p1 = floor(a/b)
+            c = sum(nums[i+1:])
+     #       print(c)
+            d = len(nums[i+1:])
+      #      print(d)
+            p2 = floor(c/d) if d != 0 else 0
+            means.append(p2)
+        print(means)
 
 
         #return 
