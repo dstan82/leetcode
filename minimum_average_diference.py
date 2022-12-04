7fe2c8023fa7fc03bf8244bef7d529ce51f24eeb
Stan Dragos <standragos82@gmail.com>
2022-12-04 09:32:49 +0200

    first attempt

--- minimum_average_diference.py
+++ minimum_average_diference.py
@@ -2,21 +2,17 @@ from math import floor
 
 class Solution:
     def minimumAverageDifference(self, nums: list[int]) -> int:
-        means = []
+        diff = {}
         for i in range(len(nums)):
             a = sum(nums[0:i+1])
             b = len(nums[0:i+1])
             p1 = floor(a/b)
             c = sum(nums[i+1:])
-     #       print(c)
             d = len(nums[i+1:])
-      #      print(d)
             p2 = floor(c/d) if d != 0 else 0
-            means.append(p2)
-        print(means)
+            diff[i] = abs(p2-p1)
+        return(min(diff, key=diff.get))
 
-
-        #return 
 
 obj = Solution()
 print(obj.minimumAverageDifference([2,5,3,9,5,3]))
